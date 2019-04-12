6b694c30840fc64e17b6ef0cb96fbbdd01ec87d8
Thuy Nhien <penhien0302@gmail.com>
2019-04-12 16:42:52 +0700

    code menu-footer-nav (partials)

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 var express = require('express');
+var hbs = require('express-handlebars');
 var app = express();
+var path = require('path');
 
 app.set('view engine', 'hbs');
 app.listen(3000);
+app.use( express.static(path.join(__dirname, 'public')));
+// app.set('views', path.join(__dirname, 'views'));
+
+
+app.engine ('hbs', hbs({
+    extname: 'hbs',
+    defaultView: 'default',
+    layoutsDir: __dirname + '/views/pages/',
+    partialsDir: __dirname + '/views/partials/'
+}));
+
+
+
+app.get('/', (req,res) => {
+    res.render('pages/index');
+})
